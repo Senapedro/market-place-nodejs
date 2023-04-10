f5526c47427c083fc65bf429596805b1fc9b5e9c
pedro sena <ph997595305@gmail.com>
2023-04-10 20:54:43 -0300

    Fix: arrumando as rotas

--- src/router/usuario.router.js
+++ src/router/usuario.router.js
@@ -9,7 +9,7 @@ router.get('/findAll', usuarioController.findAllUserController);
 // Rotas Post
 router.post('/create', usuarioController.createUserController);
 router.post('/addAddress/:id', usuarioController.addUserAddressController);
-router.post('/addFavProduct/:id', usuarioController.addUserFavProduct);
+router.post('/addFavProduct/:id', usuarioController.addUserFavProductController);
 
 // Rotas Put
 router.put('/update/:id', usuarioController.updateUserCotroller);
@@ -17,6 +17,6 @@ router.put('/update/:id', usuarioController.updateUserCotroller);
 // Rotas Delete
 router.delete('/remove/:id', usuarioController.removeUserAddressController);
 router.delete('/removeAddress', usuarioController.removeUserAddressController);
-router.delete('/removeFavProduct', usuarioController.removeUserFavProduct);
+router.delete('/removeFavProduct', usuarioController.removeUserFavProductController);
 
 module.exports = router;
